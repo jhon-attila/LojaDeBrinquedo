1e8da9adecd3a6011ccd41b8e84a66fa7470733d
joão attila <joaoattilakarpacs@gmail.com>
2025-09-21 22:45:27 -0300

    Criando as Views

--- LojaDeBrinquedos/obj/Debug/net8.0/LojaDeBrinquedos.AssemblyInfo.cs
+++ LojaDeBrinquedos/obj/Debug/net8.0/LojaDeBrinquedos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaDeBrinquedos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f84a2ab9e91bc4de15098b04b4bc64d570fa065")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e487e03e1c6b752476d167363ff3983ff76f11")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaDeBrinquedos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaDeBrinquedos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
