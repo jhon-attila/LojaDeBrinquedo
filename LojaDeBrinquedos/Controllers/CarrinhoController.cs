f3237ce70750c21ebf5e0ed99a1729c566c0b94b
joão attila <joaoattilakarpacs@gmail.com>
2025-09-21 21:52:14 -0300

    teste

---
+++ LojaDeBrinquedos/Controllers/CarrinhoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaDeBrinquedos.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- LojaDeBrinquedos/obj/Debug/net8.0/LojaDeBrinquedos.AssemblyInfo.cs
+++ LojaDeBrinquedos/obj/Debug/net8.0/LojaDeBrinquedos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaDeBrinquedos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49e493b506e9cc1391fb29fcbda5a37038d2763c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaDeBrinquedos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaDeBrinquedos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
